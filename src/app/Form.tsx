ee10800660b600d0904ab51724244dd17d62252c
永野峻輔 <peace.infi0407@gmail.com>
2024-04-15 07:05:42 +0900

    chore: update

--- src/app/Form.tsx
+++ src/app/Form.tsx
@@ -1,9 +1,14 @@
 "use client"
-import {requestValue} from './action'
+import { useFormState } from 'react-dom';
+import { requestValue } from './action'
 
 export default function Form() {
-    return <form action={requestValue}>
+    const [state, action, isPending] = useFormState(requestValue, null);
+
+    return <form action={action}>
         <input type="text" name="value" />
         <button type="submit">Request</button>
+        {isPending && <div>...loading...</div>}
+        {state && <div>Response: {JSON.stringify(state.value)}</div>}
     </form>
 }
--- src/app/action.ts
+++ src/app/action.ts
@@ -1,7 +1,6 @@
 'use server';
-import {Simulate} from "react-dom/test-utils";
 
-export async function requestValue(formData: FormData) {
+export async function requestValue(_:  {value: any;} | null, formData: FormData) {
     const value = formData.get('value');
     const res = await fetch("http://127.0.0.1:3000/api/action", {
         method: "POST",
@@ -22,7 +21,12 @@ export async function requestValue(formData: FormData) {
         return null
     })
     if (!res) {
-        return
+        return null
     }
-    console.log(await res.json())
+    const result = {
+        value: await res.json()
+    }
+    console.log(result)
+
+    return result
 }
